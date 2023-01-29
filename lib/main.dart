df7b7222bdb29546f05a4f39d1cf08321ef6ce7f
taehokang <gopae02@gmail.com>
2023-01-29 16:34:11 +0900

    간단한 쇼핑몰 기본 model , firebase 연동 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_mall/models/model_auth.dart';
 import 'package:flutter_shopping_mall/screens/screen_detail.dart';
 import 'package:flutter_shopping_mall/screens/screen_index.dart';
 import 'package:flutter_shopping_mall/screens/screen_login.dart';
 import 'package:flutter_shopping_mall/screens/screen_register.dart';
 import 'package:flutter_shopping_mall/screens/screen_search.dart';
 import 'package:flutter_shopping_mall/screens/screen_splash.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -17,17 +21,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Shopping mall',
-      routes: {
-        '/': (context) => SplashScreen(),
-        '/login': (context) => LoginScreen(),
-        '/index': (context) => IndexScreen(),
-        '/register': (context) => RegisterScreen(),
-        '/search': (context) => SearchScreen(),
-        '/detail': (context) => DetailScreen(),
-      },
-      initialRoute: '/',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => FirebaseAuthProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Shopping mall',
+        routes: {
+          '/': (context) => SplashScreen(),
+          '/login': (context) => LoginScreen(),
+          '/index': (context) => IndexScreen(),
+          '/register': (context) => RegisterScreen(),
+          '/search': (context) => SearchScreen(),
+          '/detail': (context) => DetailScreen(),
+        },
+        initialRoute: '/',
+      ),
     );
   }
 }
--- lib/screens/screen_login.dart
+++ lib/screens/screen_login.dart
@@ -4,9 +4,89 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Login'),
+      appBar: AppBar(
+        title: Text("로그인 화면"),
+      ),
+      body: Column(
+        children: [
+          EmailInput(),
+          PasswordInput(),
+          LoginButton(),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Divider(thickness: 1),
+          ),
+          RegisterButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class EmailInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+      child: TextField(
+        onChanged: (email) {},
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          labelText: '이메일',
+          helperText: '',
+        ),
+      ),
+    );
+  }
+}
+
+class PasswordInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+      child: TextField(
+        onChanged: (password) {},
+        obscureText: true,
+        decoration: InputDecoration(
+          labelText: '비밀번호',
+          helperText: '',
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.85,
+      height: MediaQuery.of(context).size.height * 0.05,
+      child: ElevatedButton (
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+        onPressed: () {},
+        child: Text('로그인'),
+      ),
+    );
+  }
+}
+
+class RegisterButton extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    final theme = Theme.of(context);
+    return TextButton (
+      onPressed: () {
+        Navigator.of(context).pushNamed('/register');
+      },
+      child: Text(
+        '이메일로 간단하게 회원가입하기',
+        style: TextStyle(color: theme.primaryColor),
       ),
     );
   }
